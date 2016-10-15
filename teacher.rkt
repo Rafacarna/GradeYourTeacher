24564af30e6dda0784d08023f755872f2f113559
Rafael Carnaval <rafaelcarna@gmail.com>
2016-10-15 19:36:09 -0300

    optimize the code

--- teacher.rkt
+++ teacher.rkt
@@ -4,72 +4,74 @@
 (require 2htdp/universe 2htdp/image)
 
 (define pgc
-  (sqlite3-connect #:database "./grade.db"))
+  (sqlite3-connect #:database "./gradeteacher.db"))
   
 
-(define (facul x nom)
-  (let* ([pgc (sqlite3-connect #:database "./grade.db")])
-  (query-exec pgc "INSERT INTO Faculdades(id , Nome) VALUES ($1, $2)" x nom) ;Faculdades = Univesities , Nome = Name
-    (displayln nom)
+(define (uni nam)
+  (let* ([pgc (sqlite3-connect #:database "./gradeteacher.db")])
+  (query-exec pgc "INSERT INTO University(Name) VALUES ($1)" nam)
+    (displayln nam)
     (disconnect pgc)))
 
-(define (area a coor)
-  (let* ([pgc (sqlite3-connect #:database "./grade.db")])
-  (query-exec pgc "INSERT INTO Area(id , Coordenacao) VALUES ($1 , $2)" a coor) ; Coordenacao = Major
-    (displayln coor)
+(define (area maj)
+  (let* ([pgc (sqlite3-connect #:database "./gradeteacher.db")])
+  (query-exec pgc "INSERT INTO Area(Major) VALUES ($1)" maj) 
+    (displayln maj)
     (disconnect pgc)))
 
-(define (prof id n m)
-  (let* ([pgc (sqlite3-connect #:database "./grade.db")])
-  (query-exec pgc "INSERT INTO Professores(id , Nome , Materia)  
-                   VALUES ($1 , $2 , $3)" id n m) ; Professores = Teachers , Materia = Class
+(define (teacher n cla)
+  (let* ([pgc (sqlite3-connect #:database "./gradeteacher.db")])
+  (query-exec pgc "INSERT INTO Teachers(Name , Class)  
+                   VALUES ($1 , $2)" n cla)
     (displayln n)
-    (displayln m)
+    (displayln cla)
     (disconnect pgc)))
 
-(define (avali nota com)
-  (let* ([pgc (sqlite3-connect #:database "./grade.db")])
-  (query-exec pgc "INSERT INTO Avaliacao(Nota , Comentario ) 
-                   VALUES ($1 , $2)" nota com) ; Comentario = Commentary , Avaliacao = Grade
-    (displayln nota)
-    (displayln com)
+(define (grade note comm)
+  (let* ([pgc (sqlite3-connect #:database "./gradeteacher.db")])
+  (query-exec pgc "INSERT INTO Grade(Note , Commentary ) 
+                   VALUES ($1 , $2)" note comm) 
+    (displayln note)
+    (displayln comm)
     (disconnect pgc)))
 
 (define (rem r)
-  (let* ([pgc (sqlite3-connect #:database "./grade.db")])
-    (query-exec pgc "DELETE FROM Faculdades WHERE id = ?" r)
+  (let* ([pgc (sqlite3-connect #:database "./gradeteacher.db")])
+    (query-exec pgc "DELETE FROM University WHERE id = ?" r)
     (disconnect pgc)))
 
 (define (rem2 r)
-  (let* ([pgc (sqlite3-connect #:database "./grade.db")])
+  (let* ([pgc (sqlite3-connect #:database "./gradeteacher.db")])
     (query-exec pgc "DELETE FROM Area WHERE id = ?" r)
     (disconnect pgc)))
 
 (define (rem3 r)
-  (let* ([pgc (sqlite3-connect #:database "./grade.db")])
-    (query-exec pgc "DELETE FROM Professores WHERE id = ?" r)
+  (let* ([pgc (sqlite3-connect #:database "./gradeteacher.db")])
+    (query-exec pgc "DELETE FROM Teachers WHERE id = ?" r)
     (disconnect pgc)))
 
 
+(define (main)
+  (displayln "GRADE YOUR TEACHER !!
 
-(displayln "GRADE YOUR TEACHER !!
+            (show 1)- To show the name of the Universities!
+            (uni name)- To add universities
+            (rem id)- To remove the university
+            (show 2)- To show the majors!
+            (area name)- To add a major
+            (rem2 id)- To remove a major
+            (show 3)- To show the teachers!
+            (teacher name class)- To add a teacher!
+            (rem3 id)- To remove a teacher
+            (show 4)- To show all notes and commentaries
+            (grade note commentary)- To add a note and a comemtary"))
 
-            1- To show the name of the Universities!
-             *To add universities type 'facul' the id and the name
-             *To remove type 'rem' and the id
-            2- To show the majors!
-             *To add a major type 'area' the id and the name
-             *To remove type 'rem2' and the id
-            3- To show the teachers!
-             *To add a teacher type 'prof' the id , the name and the class
-             *To remove type 'rem3' and the id
-            4- To show all notes and commentaries
-             *To add a note and a comemtary type 'avali' the note and the commentary")
-
-(define (menu key)
-  (cond [(eq? key 1) (displayln (query-rows pgc "SELECT * FROM Faculdades"))]
+(define (show key)
+  (cond [(eq? key 1) (displayln (query-rows pgc "SELECT * FROM University"))]
         [(eq? key 2) (displayln (query-rows pgc "SELECT * FROM Area"))]
-        [(eq? key 3) (displayln (query-rows pgc "SELECT * FROM Professores"))]
-        [(eq? key 4) (displayln (query-rows pgc "SELECT * FROM Avaliacao"))]
-        [else key]))
+        [(eq? key 3) (displayln (query-rows pgc "SELECT * FROM Teachers"))]
+        [(eq? key 4) (displayln (query-rows pgc "SELECT * FROM Grade"))]
+        [else key])
+  (main))
 
+(main)
